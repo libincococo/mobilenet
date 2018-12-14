01f320e165f5f7f6572fa16730b48fb21feae385
coco <lilybin@126.com>
2018-12-14 19:50:39 +0800

    add file

--- mobilenet.py
+++ mobilenet.py
@@ -4,8 +4,14 @@ import tensorflow as tf
 def create_variable(name,shape,initializer,dtype=tf.float32,trainable=True):
     return tf.get_variable(name,shape=shape,dtype=dtype,initializer=initializer,trainable=trainable)
 
-def conv2d():
-    pass
+def conv2d(inputs,scope,num_filters,filter_size=1,strides=1):
+    inputs_shape = inputs.get_shape()
+    in_channels = inputs_shape[-1]
+
+    with tf.variable_scope(scope):
+        filters = create_variable('filter',shape=[filter_size,filter_size,in_channels,num_filters],
+                                  initializer=tf.truncated_normal_initializer(stddev=0.01))
+        return tf.nn.conv2d(inputs,filters,strides=[1,strides,strides,1],padding="SAME")
 
 
 class MobileNet(object):
@@ -16,7 +22,6 @@ class MobileNet(object):
         self.width_multipliter = width_multipliter
         self.scope = scope
 
-
     def build_model(self):
         with tf.variable_scope(self.scope):
-            net =
+            net = conv2d(self.inputs,self.scope,)
