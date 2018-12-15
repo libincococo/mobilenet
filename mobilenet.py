175c1c411d563577c6de7653ca0d63634d4a030a
coco <lilybin@126.com>
2018-12-15 16:18:31 +0800

    add file

--- mobilenet.py
+++ mobilenet.py
@@ -80,23 +80,63 @@ class MobileNet(object):
         self.inputs = inputs
         self.num_classes=num_classes
         self.is_training = is_training
-        self.width_multipliter = width_multipliter
+        self.width_multiplier = width_multipliter
         self.scope = scope
+        self.build_model()
 
     def build_model(self):
         with tf.variable_scope(self.scope):
-            net = conv2d(self.inputs,"conv_1",round(32*width_multipliter),
+            net = conv2d(self.inputs,"conv_1",round(32*self.width_multiplier),
                          filter_size=3,strides=2)
+            self.tests = net
             net = bacthnorm(net,"conv_1/bn",is_training=self.is_training)
             net = tf.nn.relu(net,name="conv_1/relu")
 
             net = self._depthwise_separable_conv2d(net, 64, self.width_multiplier,
                                                    "ds_conv_2")  # ->[N, 112, 112, 64]
+            ###################### b.  MobileNet 核心模块 128输出 卷积步长2 尺寸减半
+            net = self._depthwise_separable_conv2d(net, 128, self.width_multiplier,
+                                                   "ds_conv_3", downsample=True)  # ->[N, 56, 56, 128]
+            ###################### c.  MobileNet 核心模块 128输出 卷积步长1 尺寸不变
+            net = self._depthwise_separable_conv2d(net, 128, self.width_multiplier,
+                                                   "ds_conv_4")  # ->[N, 56, 56, 128]
+            ###################### d.  MobileNet 核心模块 256 输出 卷积步长2 尺寸减半
+            net = self._depthwise_separable_conv2d(net, 256, self.width_multiplier,
+                                                   "ds_conv_5", downsample=True)  # ->[N, 28, 28, 256]
+            ###################### e.  MobileNet 核心模块 256输出 卷积步长1 尺寸不变
+            net = self._depthwise_separable_conv2d(net, 256, self.width_multiplier,
+                                                   "ds_conv_6")  # ->[N, 28, 28, 256]
+            ###################### f.  MobileNet 核心模块 512 输出 卷积步长2 尺寸减半
+            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier,
+                                                   "ds_conv_7", downsample=True)  # ->[N, 14, 14, 512]
+            ###################### g.  MobileNet 核心模块 512输出 卷积步长1 尺寸不变
+            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier,
+                                                   "ds_conv_8")  # ->[N, 14, 14, 512]
+            ###################### h.  MobileNet 核心模块 512输出 卷积步长1 尺寸不变
+            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier,
+                                                   "ds_conv_9")  # ->[N, 14, 14, 512]
+            ###################### i.  MobileNet 核心模块 512输出 卷积步长1 尺寸不变
+            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier,
+                                                   "ds_conv_10")  # ->[N, 14, 14, 512]
+            ###################### j.  MobileNet 核心模块 512输出 卷积步长1 尺寸不变
+            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier,
+                                                   "ds_conv_11")  # ->[N, 14, 14, 512]
+            ###################### k.  MobileNet 核心模块 512输出 卷积步长1 尺寸不变
+            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier,
+                                                   "ds_conv_12")  # ->[N, 14, 14, 512]
+            ###################### l.  MobileNet 核心模块 1024输出 卷积步长2 尺寸减半
+            net = self._depthwise_separable_conv2d(net, 1024, self.width_multiplier,
+                                                   "ds_conv_13", downsample=True)  # ->[N, 7, 7, 1024]
+            ###################### m.  MobileNet 核心模块 1024输出 卷积步长1 尺寸不变
+            net = self._depthwise_separable_conv2d(net, 1024, self.width_multiplier,
+                                                   "ds_conv_14")  # ->[N, 7, 7, 1024]
+            net = avg_pool(net,7,"avg_pool_15")
+            net = tf.squeeze(net, [1, 2], name="SpatialSqueeze")  # 去掉维度为1的维[N, 1, 1, 1024] => [N,1024]
+            self.logits = fc(net,self.num_classes,"fc_16",use_bias=True)
+            self.predictions = tf.nn.softmax(self.logits,name="softmax_17")
 
-            = self._depthwise_separable_conv2d(net, 128, self.width_multiplier,
-                                               "ds_conv_3")  # ->[N, 112, 112, 64]
 
-    def _depthwise_separable_conv2d(self,inputs,num_filters,width_multipliter,scope,dwonsample=False):
+    def _depthwise_separable_conv2d(self,inputs,num_filters,width_multiplier,scope,downsample=False):
         num_filters = round(num_filters * width_multiplier)  # 输出通道数量
         strides = 2 if downsample else 1  # 下采样 确定卷积步长
 
@@ -108,3 +148,17 @@ class MobileNet(object):
             pw_conv=conv2d(relu,"pw",num_filters=num_filters)
             bn = bacthnorm(pw_conv,"pw_bn",is_training=self.is_training)
             return tf.nn.relu(bn,name="relu2")
+
+if __name__ == "__main__":
+    # test data
+    inputs = tf.random_normal(shape=[1, 224, 224, 3],name="input")# 4张图片 224*224 大小 3通道
+    mobileNet = MobileNet(inputs)# 网络模型输出
+    graph = tf.get_default_graph()
+    writer = tf.summary.FileWriter("./logs", graph=graph)
+
+    with tf.Session() as sess:
+        init_op = tf.global_variables_initializer()
+        sess.run(init_op)
+
+        pred = sess.run(mobileNet.predictions)#预测输出
+        print(pred.shape)#打印
